f44c7bba798adc1159c89c9fcff63e1bdda32808
zync <90741920+Zyncc1402@users.noreply.github.com>
2023-12-05 19:24:24 +0530

    Update server.js PASSWORD CHANGE

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://Chandan:Chandan1402!@@cluster0.gmwqwi1.mongodb.net/?retryWrites=true&w=majority").then(() => {
+mongoose.connect("mongodb+srv://Chandan:Chandan1402@cluster0.gmwqwi1.mongodb.net/?retryWrites=true&w=majority").then(() => {
   console.log("MongoDB Connected");
 });
 
